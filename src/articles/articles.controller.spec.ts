740879d05e846d3bcb60413bdec7e22b6354fa16
Bastien Dumont <bastienxs@gmail.com>
2024-08-06 13:35:07 +0200

    test: add unit tests for controller and service

--- src/articles/articles.controller.spec.ts
+++ src/articles/articles.controller.spec.ts
@@ -1,5 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesController } from './articles.controller';
+import { ArticlesService } from './articles.service';
+
+const mockArticlesService = {
+  findAll: jest.fn(),
+  create: jest.fn(),
+  findById: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+};
+
+export const mockArticle1 = {
+  title: 'Article 1',
+  content: 'Content 1',
+  author: 'Author 1',
+  tags: ['tag1', 'tag2'],
+};
+
+const mockArticle2 = {
+  title: 'Article 2',
+  content: 'Content 2',
+  author: 'Author 2',
+  tags: ['tag3', 'tag4'],
+};
+
+export const mockArticles = [mockArticle1, mockArticle2];
 
 describe('ArticlesController', () => {
   let controller: ArticlesController;
@@ -7,6 +32,12 @@ describe('ArticlesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArticlesController],
+      providers: [
+        {
+          provide: ArticlesService,
+          useValue: mockArticlesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ArticlesController>(ArticlesController);
@@ -14,5 +45,53 @@ describe('ArticlesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return all articles', async () => {
+    mockArticlesService.findAll.mockResolvedValueOnce(mockArticles);
+
+    const articles = await controller.findAll();
+    expect(articles.length).toBe(2);
+  });
+
+  it('should create an article', async () => {
+    mockArticlesService.create.mockResolvedValueOnce(mockArticle1);
+
+    const article = await controller.create({
+      title: mockArticle1.title,
+      content: mockArticle1.content,
+      author: mockArticle1.author,
+      tags: mockArticle1.tags,
+    });
+
+    expect(article).toEqual(mockArticle1);
+  });
+
+  it('should return a single article', async () => {
+    mockArticlesService.findById.mockResolvedValueOnce(mockArticle1);
+
+    const article = await controller.findById('some-id');
+    expect(article).toEqual(mockArticle1);
+  });
+
+  it('should update an article', async () => {
+    const mockArticle = {
+      title: 'Article 1',
+      content: 'Content 1',
+      author: 'Author 1',
+      tags: ['tag1', 'tag2'],
+    };
+
+    mockArticlesService.update.mockResolvedValueOnce(mockArticle);
+
+    await controller.update('some-id', mockArticle);
+    expect(mockArticlesService.update).toHaveBeenCalled();
+  });
+
+  it('should delete an article', async () => {
+    mockArticlesService.delete.mockResolvedValueOnce(mockArticle1);
+
+    await controller.delete('some-id');
+    expect(mockArticlesService.delete).toHaveBeenCalled();
   });
 });
--- src/articles/articles.service.spec.ts
+++ src/articles/articles.service.spec.ts
@@ -1,12 +1,39 @@
+import { HttpStatus } from '@nestjs/common';
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { mockArticle1, mockArticles } from './articles.controller.spec';
+import { Article } from './articles.schema';
 import { ArticlesService } from './articles.service';
+
+class MockArticleModel {
+  constructor(dto: any) {
+    Object.assign(this, dto);
+  }
+
+  save = jest.fn().mockImplementation(() => Promise.resolve(this));
+}
+
+const mockArticleModel = Object.assign(MockArticleModel, {
+  find: jest.fn(),
+  create: jest.fn(),
+  findById: jest.fn(),
+  findByIdAndUpdate: jest.fn(),
+  update: jest.fn(),
+  deleteOne: jest.fn(),
+});
 
 describe('ArticlesService', () => {
   let service: ArticlesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticlesService],
+      providers: [
+        ArticlesService,
+        {
+          provide: getModelToken(Article.name),
+          useValue: mockArticleModel,
+        },
+      ],
     }).compile();
 
     service = module.get<ArticlesService>(ArticlesService);
@@ -14,5 +41,75 @@ describe('ArticlesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should find all articles', async () => {
+    mockArticleModel.find.mockResolvedValueOnce(mockArticles);
+    const articles = await service.findAll();
+    expect(articles.length).toEqual(2);
+  });
+
+  it('should create an article', async () => {
+    mockArticleModel.create.mockResolvedValueOnce(mockArticle1);
+    const article = await service.create({
+      title: mockArticle1.title,
+      content: mockArticle1.content,
+      author: mockArticle1.author,
+      tags: mockArticle1.tags,
+    });
+    expect(article).toBeDefined();
+  });
+
+  it('should find a single article', async () => {
+    mockArticleModel.findById.mockResolvedValueOnce(mockArticle1);
+    const article = await service.findById('some-id');
+    expect(article).toEqual(mockArticle1);
+  });
+
+  describe('update', () => {
+    it('should fail if article not found', async () => {
+      mockArticleModel.findByIdAndUpdate.mockResolvedValueOnce(null);
+      try {
+        await service.update('some-id', {});
+        expect(true).toBe(false);
+      } catch (error) {
+        expect(error.status).toBe(HttpStatus.NOT_FOUND);
+        expect(error.message).toBe('Article not found');
+      }
+    });
+
+    it('should update an article', async () => {
+      mockArticleModel.findByIdAndUpdate.mockResolvedValueOnce(mockArticle1);
+      const response = await service.update('some-id', {
+        title: 'New title',
+      });
+      expect(mockArticleModel.findByIdAndUpdate).toHaveBeenCalled();
+      expect(response).toBeDefined();
+    });
+  });
+
+  describe('delete', () => {
+    it('should fail if article not found', async () => {
+      mockArticleModel.findById.mockResolvedValueOnce(null);
+      try {
+        await service.delete('some-id');
+        expect(true).toBe(false);
+      } catch (error) {
+        expect(error.status).toBe(HttpStatus.NOT_FOUND);
+        expect(error.message).toBe('Article not found');
+      }
+    });
+
+    it('should delete an article', async () => {
+      const mockDeleteOne = jest.fn();
+      mockArticleModel.findById.mockResolvedValueOnce({
+        ...mockArticle1,
+        deleteOne: mockDeleteOne,
+      });
+      mockArticleModel.deleteOne.mockResolvedValueOnce(null);
+      await service.delete('some-id');
+      expect(mockArticleModel.findById).toHaveBeenCalled();
+      expect(mockDeleteOne).toHaveBeenCalled();
+    });
   });
 });
