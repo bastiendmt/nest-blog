72900e5c3311eaf2ad4fbce692467565b89aa237
Bastien Dumont <bastienxs@gmail.com>
2024-08-06 11:36:38 +0200

    feat(articles): delete

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,16 +1,24 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpStatus,
   Logger,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  PartialType,
+} from '@nestjs/swagger';
+import { Article } from './articles.schema';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Article } from './articles.schema';
 
 @ApiTags('articles')
 @Controller('articles')
@@ -28,6 +36,7 @@ export class ArticlesController {
   }
 
   @ApiOperation({ description: 'Create a new article' })
+  @ApiBody({ type: CreateArticleDto })
   @ApiResponse({
     status: HttpStatus.CREATED,
     description: 'Returns the created article',
@@ -54,5 +63,40 @@ export class ArticlesController {
   findById(@Param('id') id: string) {
     this.logger.log(`Finding article with id ${id}`);
     return this.articlesService.findById(id);
+  }
+
+  @ApiOperation({ description: 'Update an article by id' })
+  @ApiBody({ type: PartialType<CreateArticleDto> })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Returns the updated article',
+    type: Article,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Article not found',
+  })
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateArticleDto: Partial<CreateArticleDto>,
+  ) {
+    this.logger.log(`Updating article with id ${id}`);
+    return this.articlesService.update(id, updateArticleDto);
+  }
+
+  @ApiOperation({ description: 'Delete an article by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Article deleted',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Article not found',
+  })
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    this.logger.log(`Deleting article with id ${id}`);
+    return this.articlesService.delete(id);
   }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Article } from './articles.schema';
@@ -35,5 +35,32 @@ export class ArticlesService {
   async findById(id: string) {
     this.logger.log(`Finding article with id ${id}`);
     return this.articleModel.findById(id);
+  }
+
+  async update(id: string, updateArticleDto: Partial<Article>) {
+    this.logger.log(`Updating article with id ${id}`);
+
+    const updatedArticle = await this.articleModel.findByIdAndUpdate(
+      id,
+      { $set: updateArticleDto },
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedArticle) {
+      throw new NotFoundException(`Article not found`);
+    }
+
+    return updatedArticle;
+  }
+
+  async delete(id: string) {
+    this.logger.log(`Deleting article with id ${id}`);
+    const article = await this.articleModel.findById(id);
+
+    if (!article) {
+      throw new NotFoundException(`Article not found`);
+    }
+
+    return article.deleteOne();
   }
 }
