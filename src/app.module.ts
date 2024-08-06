b72c15beb660db22b3722a93d528128c34e9b427
Bastien Dumont <bastienxs@gmail.com>
2024-08-06 08:53:04 +0200

    feat: add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fhl2fvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
