e2cc9e3d013f7a04c4ef45950a310fb4f84134d8
Bastien Dumont <bastienxs@gmail.com>
2024-08-06 10:47:19 +0200

    feat: add healthcheck

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @ApiOperation({
+    description: 'Returns the API status, uptime (seconds) and version',
+  })
+  @Get('healthcheck')
+  getHealthCheck() {
+    return this.appService.getUpTime();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { version } from '../package.json';
 
 @Injectable()
 export class AppService {
+  private start = Date.now();
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUpTime() {
+    const now = Date.now();
+    return {
+      status: 'API Online',
+      uptime: Number((now - this.start) / 1000).toFixed(0),
+      version: version,
+    };
   }
 }
